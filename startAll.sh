c06dfe4389318c5e9abf3f7a7bc4c80cbc16007a
unknown <roma.loginovskiy@gmail.com>
2021-03-04 10:30:48 +0100

    Scripts changes

--- startAll.sh
+++ startAll.sh
@@ -1,9 +1,12 @@
-export CORECLR_PROFILER={57e1aa68-2229-41aa-9931-a6e93bbc64d8}
-export CORECLR_ENABLE_PROFILING=1
-export CORECLR_PROFILER_PATH="./libappdprofiler.so"
+export  CORECLR_PROFILER={57e1aa68-2229-41aa-9931-a6e93bbc64d8}
+export  CORECLR_ENABLE_PROFILING=1
+export  CORECLR_PROFILER_PATH=./libappdprofiler.so
+
 cd /home/centos/Gateway
-./startApp.sh
+chmod 777 StartGatewayApi.sh
+./StartGatewayApi.sh
 cd ..
 cd /home/centos/Orderfulfilment
-./startApp.sh
+chmod 777 StartOrderfulfilmentApi.sh
+./StartOrderfulfilmentApi.sh
 
